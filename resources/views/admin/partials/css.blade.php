fb41dcc2847282a8bbbdc67c1de826e389f80605
ariaschecter <aria.maulana.m@gmail.com>
2023-06-28 23:15:26 +0700

    Initial commit

---
+++ resources/views/admin/partials/css.blade.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <title>Dashcode - HTML Template</title>
+    <link rel="icon" type="image/png" href="{{ asset('backend/images/logo/favicon.svg') }}">
+    <!-- BEGIN: Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <!-- END: Google Font -->
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" href="{{ asset('backend/css/sidebar-menu.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/SimpleBar.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/app.css') }}">
+    <!-- END: Theme CSS-->
+    <script src="{{ asset('backend/js/settings.js') }}" sync></script>
+  </head>
---
+++ resources/views/admin/partials/js.blade.php
@@ -0,0 +1,11 @@
+<!-- Core Js -->
+<script src="{{ asset('backend/js/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('backend/js/popper.js') }}"></script>
+<script src="{{ asset('backend/js/tw-elements-1.0.0-alpha13.min.js') }}"></script>
+<script src="{{ asset('backend/js/SimpleBar.js') }}"></script>
+<script src="{{ asset('backend/js/iconify.js') }}"></script>
+<!-- Jquery Plugins -->
+
+<!-- app js -->
+<script src="{{ asset('backend/js/sidebar-menu.js') }}"></script>
+<script src="{{ asset('backend/js/app.js') }}"></script>
